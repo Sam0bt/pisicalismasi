39f644d6cc1f26ff488c0fcf2f2f404447455523
ali gulle <muratalgul1978@outlook.com>
2015-07-04 06:32:44 +0300

    güncelleme

--- xpyb/actions.py
+++ xpyb/actions.py
@@ -3,29 +3,34 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
-from pisi.actionsapi import pythonmodules
-#from pisi.actionsapi import pisitools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
 
-# if pisi can't find source directory, see /var/pisi/nikola/work/ and:
-# WorkDir="nikola-"+ get.srcVERSION() +"/sub_project_dir/"
+# if pisi can't find source directory, see /var/pisi/xpyb/work/ and:
+# WorkDir="xpyb-"+ get.srcVERSION() +"/sub_project_dir/"
+
+def setup():
+    shelltools.system("./autogen.sh")
+    autotools.configure()
 
 def build():
-    pythonmodules.compile()
+    autotools.make()
 
 def install():
-    pythonmodules.install()
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
 # Take a look at the source folder for these file as documentation.
 #    pisitools.dodoc("AUTHORS", "BUGS", "ChangeLog", "COPYING", "README")
 
 # If there is no install rule for a runnable binary, you can 
 # install it to binary directory.
-#    pisitools.dobin("nikola")
+#    pisitools.dobin("xpyb")
 
 # You can use these as variables, they will replace GUI values before build.
-# Package Name : nikola
-# Version : 7.6.0
-# Summary : mmmmmmmmmmmö
+# Package Name : xpyb
+# Version : 1
+# Summary : CB-based Python bindings for the X Window System
 
 # For more information, you can look at the Actions API
 # from the Help menu and toolbar.
